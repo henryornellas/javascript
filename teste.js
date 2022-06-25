ac7fae24888223a7851d7cdc01b213c3e93d0a1c
Henry Gabriel <106675861+henryornellas@users.noreply.github.com>
2022-06-25 12:40:14 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,5 @@
+var name = prompt('qual seu nome?');
+console.log(name);
+alert('muito bom ' + name + '!');
+var hobby = prompt('o que vc gosta de fazer??');
+alert ('muito bom! eu também curto ' + hobby + '!');
