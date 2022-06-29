8de6cea5b3e822a5735fc9f17673cdfc9dc65b93
Henry Gabriel <106675861+henryornellas@users.noreply.github.com>
2022-06-29 18:54:15 -0300

    Delete teste.js

--- teste.js
+++
@@ -1,5 +0,0 @@
-var name = prompt('qual seu nome?');
-console.log(name);
-alert('muito bom ' + name + '!');
-var hobby = prompt('o que vc gosta de fazer??');
-alert ('muito bom! eu também curto ' + hobby + '!');
